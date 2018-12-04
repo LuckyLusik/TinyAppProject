e83ae7e0cd1f46fa17a7a63492f3a158997b50bb
Lucy <lucyneatdesign@yahoo.ca>
2018-12-04 21:47:08 +0000

    Connected it to a front-end web form

--- express_server.js
+++ express_server.js
@@ -4,10 +4,22 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,3 +46,12 @@ app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase };
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  let randomStr = "";
+  let numLet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6; i++) {
+    randomStr += numLet.charAt(Math.floor(Math.random() * numLet.length));
+  }
+  return randomStr;
+}
